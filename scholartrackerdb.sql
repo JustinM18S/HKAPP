38f507baf53938f74fb025bfe15cbfb987f6e346
JustinM18S <juco.munar.up@phinmaed.com>
2024-10-20 19:40:15 +0800

    Add files via upload

---
+++ scholartrackerdb.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 20, 2024 at 01:34 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `scholartrackerdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `duty_logs`
+--
+
+CREATE TABLE `duty_logs` (
+  `id` int(11) NOT NULL,
+  `task` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tableuser`
+--
+
+CREATE TABLE `tableuser` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `role` enum('student','faculty','admin') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tableuser`
+--
+
+INSERT INTO `tableuser` (`id`, `email`, `password`, `username`, `role`, `created_at`) VALUES
+(1, 'angeladevera@gmail.com', '$2y$10$St7.CoIMiS8tsuJTB/wqqe/tohSDTRZIKJIHJgsv6qxc5brdwnLDy', '', 'student', '2024-10-18 10:13:21'),
+(2, 'faculty@gmail.com', '$2y$10$Oyy/lWUR9sevXPsomVNpT.oEtH1OCLbkZYIr9.t8Mtzc3MQSBDE6O', '', 'faculty', '2024-10-18 12:10:15');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `duty_logs`
+--
+ALTER TABLE `duty_logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tableuser`
+--
+ALTER TABLE `tableuser`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `duty_logs`
+--
+ALTER TABLE `duty_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tableuser`
+--
+ALTER TABLE `tableuser`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
